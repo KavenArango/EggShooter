c6952faeba3c7a445debc941a3a0b7f1ba25de3e
Kaven arango <43826797+KavenArango@users.noreply.github.com>
2020-03-24 04:01:07 -0400

    Bullets now work Removed second set of hands set up camera rig in main menu need to work on movement script for levels

--- VRGAME/Assets/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
+++ VRGAME/Assets/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
@@ -24,33 +24,32 @@ public class SimpleShoot : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (SteamVR_Actions._default.InteractUI.GetStateDown(SteamVR_Input_Sources.RightHand))
         {
-            //if (SteamVR_Actions._default.InteractUI.GetStateUp(SteamVR_Input_Sources.RightHand))
-            //{
-            Debug.Log("fire");
-            GetComponent<Animator>().SetTrigger("Fire");
+            //Debug.Log("FIRE!");
+            Shoot();
+            CasingRelease();
         }
     }
 
     void Shoot()
     {
-         GameObject bullet;
-         bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
-         bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+        GameObject bullet;
+        bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
 
         GameObject tempFlash;
-       Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
-       tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
+        //Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+        tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
 
         Destroy(tempFlash, 0.5f);
-         Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation).GetComponent<Rigidbody>().AddForce(casingExitLocation.right * 100f);
+        //Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation).GetComponent<Rigidbody>().AddForce(casingExitLocation.right * 100f);
        
     }
 
     void CasingRelease()
     {
-         GameObject casing;
+        GameObject casing;
         casing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
         casing.GetComponent<Rigidbody>().AddExplosionForce(550f, (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
         casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(10f, 1000f)), ForceMode.Impulse);
