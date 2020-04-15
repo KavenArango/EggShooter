88e45816b7c8363d68872e9027ec61de718c2960
KavenArango <kaven.arango@snhu.edu>
2020-04-15 00:08:47 -0400

    Bullet decay

--- VRGAME/Assets/Destroy.cs
+++ VRGAME/Assets/Destroy.cs
@@ -2,18 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class ApplyDamage : MonoBehaviour
+public class Destroy : MonoBehaviour
 {
     // Start is called before the first frame update
     float LifeDecay = 5.0f;
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        collision.gameObject.SendMessageUpwards("ApplyDamage", SendMessageOptions.DontRequireReceiver);
-    }
-
-
 
     void Start()
     {
