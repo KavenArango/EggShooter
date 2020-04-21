853e6e61e06cc89ea72693e1ec31b7e90784978e
Stoney0909 <49493288+Stoney0909@users.noreply.github.com>
2020-04-21 18:42:14 -0400

    Changed some part of the scenes

--- VRGAME/Assets/stuff/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
+++ VRGAME/Assets/stuff/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
@@ -47,8 +47,8 @@ public class SimpleShoot : MonoBehaviour
 
 
 
-        //if (SteamVR_Actions._default.GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (SteamVR_Actions._default.GrabPinch.GetStateDown(SteamVR_Input_Sources.RightHand))
+        //if(Input.GetKeyDown(KeyCode.Space))
         {
             if (currentAmmo > 0)
             {
